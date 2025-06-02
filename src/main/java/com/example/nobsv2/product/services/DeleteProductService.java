532a91a597e2e8c15cbca995d26fdac5b212a83e
RanXom <saiyedshizain@gmail.com>
2025-06-02 23:59:42 +0530

    refactor(service): implement actual deletion logic in DeleteProductService

--- src/main/java/com/example/nobsv2/product/services/DeleteProductService.java
+++ src/main/java/com/example/nobsv2/product/services/DeleteProductService.java
@@ -1,14 +1,32 @@
 package com.example.nobsv2.product.services;
 
 import com.example.nobsv2.Command;
+import com.example.nobsv2.product.ProductRepository;
+import com.example.nobsv2.product.model.Product;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
-public class DeleteProductService implements Command<Void, String> {
+public class DeleteProductService implements Command<Integer, Void> {
+
+    private final ProductRepository productRepository;
+
+    public DeleteProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
     @Override
-    public ResponseEntity<String> execute(Void input) {
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Product deletes");
+    public ResponseEntity<Void> execute(Integer id) {
+        Optional<Product> product = productRepository.findById(id);
+
+        if (product.isPresent()) {
+            productRepository.deleteById(id);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }
+
+        return null;
     }
 }
